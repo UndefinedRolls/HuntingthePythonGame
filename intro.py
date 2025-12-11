30f6fd27d1d73f50538b9aae8fd7975888afdf41
UndefinedRolls <undefinedrolls@gmail.com>
2025-12-11 16:58:02 -0700

    intro

---
+++ intro.py
@@ -0,0 +1,65 @@
+def get_action_target(text):
+    temp = text.lower().strip().split()
+    if not temp:
+        return None, None
+    action = temp[0]
+    if len(temp) == 1:
+        target = None
+    elif len(temp) > 2:
+        target = " ".join(temp[1:])
+    else:
+        target = temp[1]
+    return action, target
+
+def intro(player):
+    print("You stand in the doorway of a ramshackle tavern in a ramshackle town.  The room is "
+    "mostly dark, but motes of dust float on a few bars of light that comes in from the grimy windows."
+    "  There is a long bar in the back of the room, stairs up to the proprietors rooms blocks by "
+          " a rope, a handful of occupied tables, and an open archway that leads to a storage room "
+          "in the back.")
+
+    while True:
+        print("What do you do?")
+        choice = input(">>").strip()
+        if not choice:
+            print("You stand and stare blankly into the room.  The room quiets for a moment "
+                  "wondering what you are doing, then everyone goes back to what they"
+                  "were doing.")
+            continue
+
+        action, target = get_action_target(choice)
+
+        match action:
+            case "look" | "examine" | "l":
+                handle_look(player, target)
+            case "go" | "move" | "walk":
+                handle_move(player, target)
+            case "talk" | "speak":
+                handle_talk(player, target)
+            case "help" | "?":
+                show_help()
+            case "quit" | "exit" | "q":
+                return "quit"
+            case _:
+                print (f"{action} isn't valid here.  Type 'help' for commands")
+
+def handle_look(player, target):
+    result = player.roll_check("wisdom")
+    if result > 5:
+        print("\nThe air smells of stale alcohol and body odor.")
+    if result > 10 and ("bar" in target or "bartender" in target):
+        print("The bar is long, wooden, covered in grime.  Behind it stands an orcish woman"
+                      " with long black hair and scowl.  She won't meet your eyes.  There is a short "
+                      "menu claiming they serve ale, wine and spirits.  They do not have a kitchen.")
+    if result > 15 and ("bar" in target or "bartender" in target):
+            print("The bartender is casting dark looks at a table in the far corner where a "
+                      "human woman sits nursing a drink in a pewter mug.  She cringes every time "
+                      "the table to her left gets loud.")
+    elif result > 15:
+        print("You notice the bartender casting dark looks at a corner table.")
+    elif "bar" not in target and "bartender" not in target:
+        print("You see:\n"
+              "- A long bar in the back\n"
+              "- Stairs blocked by a rope\n"
+              "- Several occupied tables\n"
+              "- An archway to a storage room\n")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import character
 import webcolors
 import sys
+import intro
+
 def parse_color(color):
     color = color.replace(" ", "").lower()
     try:
@@ -15,10 +17,9 @@ def parse_color(color):
         return hex_, None
     except ValueError:
         return "#000000", color
-
-def main():
-    char_name = input(f"What is you name?\n" )
-    char_color, clr_txt = parse_color(input(f"What is your favorite color?\n" ))
+def create_character():
+    char_name = input(f"What is you name?\n")
+    char_color, clr_txt = parse_color(input(f"What is your favorite color?\n"))
     char_wsv = input(f"What is the wind speed velocity of a laden swallow?\n")
 
     if "african or european swallow" in char_wsv.lower() or char_wsv == "":
@@ -29,6 +30,49 @@ def main():
 
     new_player = character.Player(char_name, char_color, clr_txt)
     print(new_player)
+    return new_player
+def menu(save_data):
+    if save_data:
+        show_continue = "Continue"
+    else:
+        show_continue = "No Save Found"
+
+    print(f"/^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  \n"
+          f"|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|  \n"
+          f"| |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  \n"
+          f"|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|  \n"
+          f"| |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  \n"
+          f"| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |  \n"
+          f"              ╦ ╦┌─┐┬  ┌─┐┌─┐┌┬┐┌─┐   ╔╦╗┬─┐┌─┐┬  ┬┌─┐┬  ┌─┐┬─┐                 \n"
+          f"              ║║║├┤ │  │  │ ││││├┤     ║ ├┬┘├─┤└┐┌┘├┤ │  ├┤ ├┬┘                 \n"
+          f"              ╚╩╝└─┘┴─┘└─┘└─┘┴ ┴└─┘┘   ╩ ┴└─┴ ┴ └┘ └─┘┴─┘└─┘┴└─                 \n"
+          f"/^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  /^\\  \n"
+          f"|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|==|^|  \n"
+          f"| |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  \n"
+          f"|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|--|o|  \n"
+          f"| |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  | |  \n"
+          f"| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |==| |  \n"
+          f"       ··············                                                           \n"
+          f"       :    MENU    :                                                           \n"
+          f"       ··············                                                           \n")
+    print(f"1: New Game\n"
+          f"2: {show_continue}\n"
+          f"3: Exit")
+    return input(">>")
+
+def main(save_data=None):
+    choice = menu(save_data)
+    match choice:
+        case "1":
+            print("Create your character")
+            player = create_character()
+        case "2":
+            if save_data:
+                pass
+            else:
+                pass
+        case "3":
+            sys.exit()
 
 if __name__ == '__main__':
     main()
